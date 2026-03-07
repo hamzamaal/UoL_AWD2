44a5ad8381a04b249fa75f8b331f4443f209510f
Hamza Maal <hamza.maal@gmail.com>
2026-03-07 22:17:05 +0200

    New comments

--- MindTopia/accounts/admin.py
+++ MindTopia/accounts/admin.py
@@ -1,6 +1,12 @@
+# Import Django's built-in admin module used to register models in the admin interface
 from django.contrib import admin
+
+# Import the UserProfile model from the current application's models
 from .models import UserProfile
 
+# Register the UserProfile model with the Django admin site using a decorator
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
+
+    # Specify which fields should be displayed in the admin list view for UserProfile records
     list_display = ('user', 'first_name', 'last_name', 'role', 'image')
--- MindTopia/accounts/apps.py
+++ MindTopia/accounts/apps.py
@@ -1,8 +1,17 @@
+# Import AppConfig which is used to configure application-specific settings in Django
 from django.apps import AppConfig
 
+# Define the configuration class for the 'accounts' application
 class AccountsConfig(AppConfig):
+
+    # Specify the default primary key field type used for models in this app
     default_auto_field = 'django.db.models.BigAutoField'  # Django 4.2+ Compatibility
+
+    # Define the name of the Django application
     name = 'accounts'
 
+    # The ready() method runs when the Django application is fully loaded
     def ready(self):
+
+        # Import the signals module to ensure signal handlers are registered when the app starts
         import accounts.signals  # Ensures signals are loaded properly
